689cf2f67aa88da229eba5ed70e86d942740ecf3
Arkoprabho Dasgupta <apdg2407@gmail.com>
2024-11-12 12:36:59 -0500

    valve change

---
+++ src/prommis/solvent_extraction/sx_with_valve.py
@@ -0,0 +1,68 @@
+from pyomo.environ import (
+    ConcreteModel,
+    SolverFactory,
+    units,
+    TransformationFactory,
+    Var,
+    value,
+    log10,
+    Suffix,
+    Constraint
+)
+from pyomo.dae.flatten import flatten_dae_components
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from idaes.core import FlowDirection, FlowsheetBlock, MaterialBalanceType
+from idaes.core.util import from_json, DiagnosticsToolbox
+from idaes.core.scaling import set_scaling_factor, CustomScalerBase, report_scaling_factors
+from idaes.core.solvers import get_solver
+from idaes.models.unit_models import Valve
+from idaes.models.control.controller import (
+    PIDController,
+    ControllerType,
+    ControllerMVBoundType,
+)
+
+from prommis.leaching.leach_solution_properties import LeachSolutionParameters
+from prommis.solvent_extraction.ree_og_distribution import REESolExOgParameters
+from prommis.solvent_extraction.hybrid_solvent_extraction import SolventExtraction
+from prommis.solvent_extraction.D_variant_model import D_calculation
+
+m = ConcreteModel()
+
+time_duration = 24
+
+m.fs = FlowsheetBlock(dynamic=True, time_set=[0, time_duration], time_units=units.hour)
+
+m.fs.prop_o = REESolExOgParameters()
+m.fs.leach_soln = LeachSolutionParameters()
+
+number_of_stages = 3
+
+m.fs.solex = SolventExtraction(
+    number_of_finite_elements=number_of_stages,
+    aqueous_stream={
+        "property_package": m.fs.leach_soln,
+        "flow_direction": FlowDirection.forward,
+        "has_energy_balance": False,
+        "has_pressure_balance": False,
+    },
+    organic_stream={
+        "property_package": m.fs.prop_o,
+        "flow_direction": FlowDirection.backward,
+        "has_energy_balance": False,
+        "has_pressure_balance": False,
+    },
+)
+
+m.fs.valve = Valve(
+        dynamic=False,
+        has_holdup=False,
+        material_balance_type=MaterialBalanceType.componentTotal,
+        property_package=m.fs.leach_soln,
+    )
+
+m.fs.control = PIDController(process_var=m.fs.solex.mscontactor.volume_frac_stream[:, 3, "aqueous"])
